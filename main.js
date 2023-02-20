328973e786fa76885e466fa069ca783c51d3b3b1
avillaq <villafuertequispealex@gmail.com>
2023-02-20 01:51:28 -0500

    Cambiamos el color de fonde de algunos elementos

--- main.js
+++ main.js
@@ -1,71 +1,89 @@
-//---------------------Metodo antiguo para consumir una api - (linea 2 - 32)------------------------------------
-/*const sfw = document.querySelector(".selectSFW");
-const nsfw = document.querySelector(".selectNSFW");
-
-const botonVerSFW = document.getElementById('botonVerSFW');
-const botonVerNSFW = document.getElementById('botonVerNSFW');
-
-//Como consumir API: https://www.youtube.com/watch?v=2Xm9P_tXtK8
-//URL de la API : https://api.waifu.pics
-const xhr = new XMLHttpRequest();
-
-function onRequestHandler(){
-    if(this.readyState == 4 && this.status == 200){
-        const data = JSON.parse(this.response);
-        //Obtener valor de un Json: https://es.stackoverflow.com/questions/9478/c%C3%B3mo-puedo-leer-un-campo-espec%C3%ADfico-dentro-de-un-objeto-json-que-se-encuentra-e
-        //console.log(data['url']);
-        document.getElementById("campoImage").src = data['url']; 
-        document.getElementById("campoLink").href = data['url']; 
+document.addEventListener('DOMContentLoaded', init);
+function init() {
+    //---------------------Metodo antiguo para consumir una api - (linea 2 - 32)------------------------------------
+    /*const sfw = document.querySelector(".selectSFW");
+    const nsfw = document.querySelector(".selectNSFW");
+    
+    const botonVerSFW = document.getElementById('botonVerSFW');
+    const botonVerNSFW = document.getElementById('botonVerNSFW');
+    
+    //Como consumir API: https://www.youtube.com/watch?v=2Xm9P_tXtK8
+    //URL de la API : https://api.waifu.pics
+    const xhr = new XMLHttpRequest();
+    
+    function onRequestHandler(){
+        if(this.readyState == 4 && this.status == 200){
+            const data = JSON.parse(this.response);
+            //Obtener valor de un Json: https://es.stackoverflow.com/questions/9478/c%C3%B3mo-puedo-leer-un-campo-espec%C3%ADfico-dentro-de-un-objeto-json-que-se-encuentra-e
+            //console.log(data['url']);
+            document.getElementById("campoImage").src = data['url']; 
+            document.getElementById("campoLink").href = data['url']; 
+        }
     }
-}
-function mostrarImageSFW(){
-    xhr.addEventListener("load", onRequestHandler);
-    xhr.open("GET", 'https://api.waifu.pics/sfw/'+sfw.value);
-    xhr.send();
-}
-function mostrarImageNSFW(){   
-    xhr.addEventListener("load", onRequestHandler);
-    xhr.open("GET", 'https://api.waifu.pics/nsfw/'+nsfw.value);
-    xhr.send();
-}
-botonVerSFW.addEventListener('click', mostrarImageSFW);
-botonVerNSFW.addEventListener('click', mostrarImageNSFW);*/
+    function mostrarImageSFW(){
+        xhr.addEventListener("load", onRequestHandler);
+        xhr.open("GET", 'https://api.waifu.pics/sfw/'+sfw.value);
+        xhr.send();
+    }
+    function mostrarImageNSFW(){   
+        xhr.addEventListener("load", onRequestHandler);
+        xhr.open("GET", 'https://api.waifu.pics/nsfw/'+nsfw.value);
+        xhr.send();
+    }
+    botonVerSFW.addEventListener('click', mostrarImageSFW);
+    botonVerNSFW.addEventListener('click', mostrarImageNSFW);*/
 
-//---------------------Metodo con async y fetch para consumir una api - (linea 36 - 69)------------------------------------
- 
-const sfw = document.querySelector(".selectSFW");
-const nsfw = document.querySelector(".selectNSFW");
+    //---------------------Metodo con async y fetch para consumir una api - (linea 36 - 69)------------------------------------
 
-const botonVerSFW = document.getElementById('botonVerSFW');
-const botonVerNSFW = document.getElementById('botonVerNSFW');
+    const sfw = document.querySelector(".selectSFW");
+    const nsfw = document.querySelector(".selectNSFW");
 
-//Referencia : https://www.youtube.com/watch?v=PNr8-JDMinU
-const cargarLink = async (type, tag) =>{
-    try {
-        const respuesta = await fetch('https://api.waifu.pics/'+type+'/'+tag);
-        //console.log(respuesta);
+    const botonVerSFW = document.getElementById('botonVerSFW');
+    const botonVerNSFW = document.getElementById('botonVerNSFW');
 
-        if(respuesta.status === 200){
-            const datos = await respuesta.json();
-            //console.log(datos.url);
-            document.getElementById("campoImage").src = datos.url; 
-            document.getElementById("campoLink").href = datos.url; 
+    //Referencia : https://www.youtube.com/watch?v=PNr8-JDMinU
+    const cargarLink = async (type, tag) => {
+        try {
+            const respuesta = await fetch('https://api.waifu.pics/' + type + '/' + tag);
+            //console.log(respuesta);
 
-            console.log(datos.url);
+            if (respuesta.status === 200) {
+                const datos = await respuesta.json();
+                //console.log(datos.url);
+                document.getElementById("campoImage").src = datos.url;
+                document.getElementById("campoLink").href = datos.url;
 
+                console.log(datos.url);
+
+            }
+
+        } catch (error) {
+            console.log(error);
         }
 
-    } catch (error) {
-        console.log(error);
-    }   
+    }
+    //botonVerSFW.addEventListener('click', cargarLink(sfw.value)); ERROR: Hacerlo de esta manera, ejecuta la funcion cargarlink() sin presionar el boton 
+    botonVerSFW.addEventListener('click', () => {
+        cargarLink('sfw', sfw.value)
+    });
+    botonVerNSFW.addEventListener('click', () => {
+        cargarLink('nsfw', nsfw.value)
+    });
+
+    /**Mostraremos la lista de categorias de acuerdo al tipo de elija*/
+    const typeSFW = document.querySelector("#typeSFW");
+    const typeNSFW = document.querySelector("#typeNSFW");
+
+    typeSFW.addEventListener('click', function(){
+        
+    })
+
+
+
+
+
+
+
+
 
 }
-//botonVerSFW.addEventListener('click', cargarLink(sfw.value)); ERROR: Hacerlo de esta manera, ejecuta la funcion cargarlink() sin presionar el boton 
-botonVerSFW.addEventListener('click', () => {
-    cargarLink('sfw', sfw.value)
-});
-botonVerNSFW.addEventListener('click', () => {
-    cargarLink('nsfw', nsfw.value)
-});
-
-/** */
