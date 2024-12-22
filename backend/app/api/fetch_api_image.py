3cc9ee7cc6ad10dfac8faf4179319fc9f97989e8
avillaq <villafuertequispealex@gmail.com>
2024-12-22 13:25:28 -0500

    Add fetch_image function to retrieve images from waifu.im and waifu.pics APIs

---
+++ backend/app/api/fetch_api_image.py
@@ -0,0 +1,109 @@
+import requests
+import random
+IMAGES_TAGS = { 
+    "waifu.im": {
+        "sfw": [
+            "maid",
+            "waifu",
+            "marin-kitagawa",
+            "mori-calliope",
+            "raiden-shogun",
+            "oppai",
+            "selfies",
+            "uniform",
+            "kamisato-ayaka"
+        ],
+        "nsfw": [
+            "ass",
+            "hentai",
+            "milf",
+            "oral",
+            "paizuri",
+            "ecchi",
+            "ero"
+        ]
+    },
+    "waifu.pics": {   
+        "sfw": [
+            "waifu",
+            "neko",
+            "shinobu",
+            "megumin",
+            "bully",
+            "cuddle",
+            "cry",
+            "hug",
+            "awoo",
+            "kiss",
+            "lick",
+            "pat",
+            "smug",
+            "bonk",
+            "yeet",
+            "blush",
+            "smile",
+            "wave",
+            "highfive",
+            "handhold",
+            "nom",
+            "bite",
+            "glomp",
+            "slap",
+            "kill",
+            "kick",
+            "happy",
+            "wink",
+            "poke",
+            "dance",
+            "cringe"
+        ], 
+        "nsfw": [
+            "waifu",
+            "neko",
+            "trap",
+            "blowjob"
+        ]
+    }
+}
+
+
+WAIFU_IM_API_URL = "https://api.waifu.im/search"
+WAIFU_PICS_API_URL = "https://api.waifu.pics"
+
+
+def fetch_image(tag="waifu", type="sfw"):
+    api_source = random.randint(0, 1)
+    print(f"API Source: {api_source}")
+    if api_source == 0 and IMAGES_TAGS["waifu.im"][type].count(tag) > 0:
+        params = {
+            "included_tags": [tag],
+            "height": "<=750",
+            "is_nsfw": type == "nsfw"
+        }
+        response = requests.get(WAIFU_IM_API_URL, params=params)
+
+        if response.status_code == 200:
+            data = response.json()
+            return {
+                "image_url": data["images"][0]["url"],
+                "source": WAIFU_IM_API_URL
+            }
+        return {
+            "error": "Failed to fetch image"
+        }
+
+    elif api_source == 1 and IMAGES_TAGS["waifu.pics"][type].count(tag) > 0:
+        response = requests.get(f"{WAIFU_PICS_API_URL}/{type}/{tag}")
+
+        if response.status_code == 200:
+            data = response.json()
+            return {
+                "image_url": data["url"],
+                "source": WAIFU_PICS_API_URL
+            }
+
+        return {
+            "error": "Failed to fetch image"
+        }
+
+
