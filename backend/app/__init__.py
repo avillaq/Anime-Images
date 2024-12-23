f59879e1ea3b35b7b53872170a71a0d6d919c08f
avillaq <villafuertequispealex@gmail.com>
2024-12-23 03:34:25 -0500

    Refactor app initialization to include blacklist check in JWT guard and update CORS configuration

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from app.extensions import db, limiter, cache, cors, guard
+from app.extensions import db, limiter, cache, cors, guard, blacklist
 from app.api.models import User
 
 def create_app():
@@ -8,11 +8,22 @@ def create_app():
 
     db.init_app(app)
     limiter.init_app(app)
-    cache.init_app(app)
-    cors.init_app(app, resources={r"/api/*": {"origins": "*"}})
+    cache.init_app(app, config = {
+        "CACHE_TYPE": "SimpleCache"
+    })
+    cors.init_app(app, 
+                resources=r"/api/*",
+                origins=["*"],
+                methods=["GET", "POST", "PUT", "DELETE"],
+                allow_headers = ["Content-Type", "Authorization"],
+                expose_headers = ["Content-Range", "X-Content-Range"],
+                supports_credentials=False,
+        )
 
+    def is_blacklisted(jti):
+        return jti in blacklist
 
-    guard.init_app(app, User)
+    guard.init_app(app, User,is_blacklisted=is_blacklisted)
 
     # Register blueprints
     from app.api import bp as api_bp
