452736360dcd4d60836e29fee52c0aeaaea9ad2e
avillaq <villafuertequispealex@gmail.com>
2024-12-23 13:43:04 -0500

    feat: implement RouterProvider in App component and enhance Layout with navigation links

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
-function App() {
-  return (
-    <>
+import { RouterProvider } from './routes/RouterProvider'
 
-    </>
-  )
+function App() {
+  return <RouterProvider></RouterProvider>
 }
 
 export default App
--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,6 +1,16 @@
 
 export const Layout = () => {
   return (
-    <div>Layout</div>
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+          </ul>
+        </nav>
+      </header>
+
+    </div>
   )
 }
