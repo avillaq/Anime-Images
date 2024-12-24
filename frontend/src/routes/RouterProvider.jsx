7a0260f85e08b38d4e173c219e24ce4db6b2c460
avillaq <villafuertequispealex@gmail.com>
2024-12-24 12:23:36 -0500

    feat: integrate NextUIProvider into RouterProvider for enhanced routing and navigation

--- frontend/src/routes/RouterProvider.jsx
+++ frontend/src/routes/RouterProvider.jsx
@@ -1,21 +1,34 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useNavigate, useHref } from 'react-router-dom';
+import { NextUIProvider } from "@nextui-org/system";
 import { routes } from './Routes';
 import { Layout } from '../components/Layout';
 
+
+const NextUIRouteProvider = () => {
+  const navigate = useNavigate();
+
+  return (
+    <NextUIProvider navigate={navigate} useHref={useHref}>
+      <Routes>
+        <Route element={<Layout />}>
+          {routes.map(({ path, element: Element }) => (
+            <Route
+              key={path}
+              path={path}
+              element={<Element />}
+            />
+          ))}
+        </Route>
+      </Routes>
+    </NextUIProvider>
+  )
+}
+
 export const RouterProvider = () => {
+
   return (
     <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            {routes.map(({ path, element: Element }) => (
-              <Route
-                key={path}
-                path={path}
-                element={<Element />}
-              />
-            ))}
-          </Route>
-        </Routes>
+      <NextUIRouteProvider/>
     </BrowserRouter>
   );
 };
