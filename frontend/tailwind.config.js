da1c364057ea0d2a0600419ba943d8a3e7c07a48
avillaq <villafuertequispealex@gmail.com>
2024-12-24 12:11:37 -0500

    feat: add NextUI dependencies and configure Tailwind CSS with NextUI plugin

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,16 @@
+const {nextui} = require("@nextui-org/theme");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/components/button.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 }
 
