a7cf4ea59d97755f009c155153dc02d40a7d112d
avillaq <villafuertequispealex@gmail.com>
2024-12-24 20:38:30 -0500

    feat: enhance Home component with header, buttons, and improved styling

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,30 @@
+import "../styles/Home.css";
+import {Button} from "@nextui-org/button";
 
 export const Home = () => {
   return (
     <aside className="home-container">
+      <header>
+        <h1>Random Anime Images</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque suscipit dicta assumenda iste doloribus ullam sit tempora ut excepturi placeat? Officiis qui ducimus debitis. Quisquam sed porro facilis possimus quis!
+        </p>
+      </header>
+      <main>
+        <div className="home-button-container">
+          <Button color="secondary">Get sfw Image</Button>
+          <Button color="danger">Get nsfw Image</Button>
+        </div>
+        <div className="home-image-container">
+          <img src="" alt="Anime image" />
+        </div>
+
+      </main>
+
+
+
+
+
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque suscipit dicta assumenda iste doloribus ullam sit tempora ut excepturi placeat? Officiis qui ducimus debitis. Quisquam sed porro facilis possimus quis!
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque suscipit dicta assumenda iste doloribus ullam sit tempora ut excepturi placeat? Officiis qui ducimus debitis. Quisquam sed porro facilis possimus quis!
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque suscipit dicta assumenda iste doloribus ullam sit tempora ut excepturi placeat? Officiis qui ducimus debitis. Quisquam sed porro facilis possimus quis!
