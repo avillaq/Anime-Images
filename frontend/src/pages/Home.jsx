d03952fbdc7e23f95379e0a863af32d348d7df53
avillaq <villafuertequispealex@gmail.com>
2024-12-25 01:45:35 -0500

    feat: restructure Home component layout and update styles for better organization and responsiveness

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,33 +1,27 @@
 import "../styles/Home.css";
-import {Button} from "@nextui-org/button";
-import {Image} from "@nextui-org/image";
+import { Button } from "@nextui-org/button";
+import { Image } from "@nextui-org/image";
 import WelcomeImage from "../assets/anime-maid.webp";
 
 export const Home = () => {
   return (
-    <aside className="home-container">
-      <header>
+    <section className="home-container">
+      <div className="home-content-container">
         <h1>Random Anime Images</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque suscipit dicta assumenda iste doloribus ullam sit tempora ut excepturi placeat? Officiis qui ducimus debitis. Quisquam sed porro facilis possimus quis!
         </p>
-      </header>
-      <main>
         <div className="home-button-container">
-          <Button color="secondary">Get sfw Image</Button>
-          <Button color="danger">Get nsfw Image</Button>
+          <Button color="secondary">Get anime 1 Image</Button>
+          <Button color="danger">Get Anime 2 Image</Button>
         </div>
-        <div className="home-image-container">
+      </div>
+      <figure className="home-image-container">
         <Image
           alt="Anime Image"
           src={WelcomeImage}
         />
-        </div>
-
-      </main>
-
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque suscipit dicta assumenda iste doloribus ullam sit tempora ut excepturi placeat? Officiis qui ducimus debitis. Quisquam sed porro facilis possimus quis!
-      
-    </aside>
+      </figure>
+    </section>
   )
 }
