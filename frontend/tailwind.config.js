519efe87cead58d55edbf59bbce4480ee922b846
avillaq <villafuertequispealex@gmail.com>
2024-12-26 01:41:14 -0500

    feat: add @nextui-org/spinner package and update Tailwind config for component inclusion

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/components/(button|navbar|image|link|select|listbox|skeleton|modal|input|form).js",
+    "./node_modules/@nextui-org/theme/dist/components/(button|navbar|image|link|select|listbox|skeleton|modal|input|form|spinner).js",
   ],
   theme: {
     extend: {},
