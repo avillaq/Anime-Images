2d06d20ca534c15fc37f38bdcdcd9d832f360d6d
avillaq <villafuertequispealex@gmail.com>
2024-12-27 12:59:32 -0500

    feat: refactor axios interceptors to improve token handling and setup

--- frontend/src/service/axiosInstance.js
+++ frontend/src/service/axiosInstance.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-import jwtDecode from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 import { API_CONFIG } from "../config/api.config";
-import { useAuthStore } from "../store/authStore";
 
 export const axiosInstance = axios.create({
   baseURL: API_CONFIG.baseUrl,
@@ -10,29 +9,31 @@ export const axiosInstance = axios.create({
   },
 });
 
-axiosInstance.interceptors.request.use(
-  async (config) => {
-    const { access_token, setAccessToken, logout } = useAuthStore();
-
-    if (access_token) {
-      const tokenData = jwtDecode(access_token);
-      const isExpired = tokenData.exp * 1000 < Date.now();
-      if (isExpired) {
-        try {
-          const response = await axios.post("/auth/refresh", {
-            token: refreshToken
-          });
-          setAccessToken(response.data.access_token);
-          config.headers.Authorization = `Bearer ${response.data.access_token}`;
-        } catch (error) {
-          logout();
-          window.location.href = "/login";
+export const setupInterceptors = (useAuthStore) => {
+  axiosInstance.interceptors.request.use(
+    async (config) => {
+      const { access_token, setAccessToken, logout } = useAuthStore.getState();
+  
+      if (access_token) {
+        const tokenData = jwtDecode(access_token);
+        const isExpired = tokenData.exp * 1000 < Date.now();
+        if (isExpired) {
+          try {
+            const response = await axios.post("/auth/refresh", {
+              token: refreshToken
+            });
+            setAccessToken(response.data.access_token);
+            config.headers.Authorization = `Bearer ${response.data.access_token}`;
+          } catch (error) {
+            logout();
+            window.location.href = "/login";
+          }
+        } else {
+          config.headers.Authorization = `Bearer ${access_token}`;
         }
-      } else {
-        config.headers.Authorization = `Bearer ${access_token}`;
       }
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+      return config;
+    },
+    (error) => Promise.reject(error)
+  );
+}
