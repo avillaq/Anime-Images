04ac87f78b64b9a81b2b505ae07a5addd99b3fce
avillaq <villafuertequispealex@gmail.com>
2024-12-27 11:58:33 -0500

    feat: implement API service functions for authentication and image retrieval

---
+++ frontend/src/service/apiService.js
@@ -0,0 +1,40 @@
+import axiosInstance from "./axiosInstance";
+import { API_CONFIG } from "../config/api.config";
+
+export const login = async (username, password) => {
+  try {
+    const response = await axiosInstance.post(API_CONFIG.endpoints.auth.login, { username, password });
+    return response.data;
+  } catch (error) {
+    return { error: error.response.data };
+  }
+};
+
+export const register = async (username, password) => {
+  try {
+    const response = await axiosInstance.post(API_CONFIG.endpoints.auth.register, { username, password });
+    return response.data;
+  } catch (error) {
+    return { error: error.response.data };
+  }
+};
+
+export const fetchRandomImage = async (type, tag) => {
+  try {
+    const response = await axiosInstance.post(API_CONFIG.endpoints.images.getRandom, { type, tag });
+    return response.data;
+  } catch (error) {
+    return { error: error.response.data };
+  }
+};
+
+export const fetchTags = async () => {
+  try {
+    const response = await axiosInstance.get(API_CONFIG.endpoints.images.tags);
+    return response.data;
+  } catch (error) {
+    return { error: error.response.data };
+  }
+};
+
+// TODO: Implement more API service functions 
