7b2cd4ace180f845933f62d6848fbd4f8a03caa4
avillaq <villafuertequispealex@gmail.com>
2025-01-09 12:39:43 -0500

    feat: add favorites page with fetch functionality and improved layout

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,6 +1,26 @@
+import { useEffect } from "react";
+import { fetchFavorites } from "../service/apiService";
 
+import "../styles/Favorites.css";
 export const Favorites = () => {
+  useEffect(() => {
+    fetchFavorites().then((data) => {
+      console.log(data);
+    });
+  }, []);
+  
   return (
-    <div>Favorites</div>
+    <section className="favorites-container">
+      <div className="favorites-header-container">
+        <h1>All your favorite images</h1>
+      </div>
+      <div className="favorites-content-container">
+        {
+          /*
+            Here you can map over the favorite images and display them in a grid.
+          */
+        }
+      </div>
+    </section>
   )
 }
