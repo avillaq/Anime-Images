c5420cee7cf5212b89de0af3686c698ee34b3685
avillaq <villafuertequispealex@gmail.com>
2025-01-13 11:05:05 -0500

    feat: update database URI in .env.example and add Flask-Migrate for database migrations

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from app.extensions import db, limiter, cache, cors, guard, blacklist
 from app.api.models import User
+from flask_migrate import Migrate
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("config.Config")
 
     db.init_app(app)
+    Migrate(app, db)
     limiter.init_app(app)
     cache.init_app(app, config = {
         "CACHE_TYPE": "SimpleCache"
